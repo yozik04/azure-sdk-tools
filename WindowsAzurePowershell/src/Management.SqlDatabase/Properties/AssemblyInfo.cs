b072bbd297e5f30848aabe49e457ff26cb1e1af7
James Tao <jamestao@microsoft.com>
2012-06-11 12:17:10 -0700

    Fixing project files to match new naming convention

--- WindowsAzurePowershell/src/Management.SqlDatabase/Properties/AssemblyInfo.cs
+++ WindowsAzurePowershell/src/Management.SqlDatabase/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ManagementTools.PowerShell.SqlDB")]
+[assembly: AssemblyTitle("Management.SqlDatabase")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ManagementTools.PowerShell.SqlDB")]
+[assembly: AssemblyProduct("Management.SqlDatabase")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
