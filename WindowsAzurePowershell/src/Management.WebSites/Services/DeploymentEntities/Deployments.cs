8dd06f35d72df8fdeb3959bb6bfbae760efd635e
Andre Rodrigues <andrerod@microsoft.com>
2012-09-18 15:51:29 +0100

    Adding deployment contract data members.

--- WindowsAzurePowershell/src/Management.WebSites/Services/DeploymentEntities/Deployments.cs
+++ WindowsAzurePowershell/src/Management.WebSites/Services/DeploymentEntities/Deployments.cs
@@ -14,6 +14,7 @@
 
 namespace Microsoft.WindowsAzure.Management.Websites.Services.DeploymentEntities
 {
+    using System;
     using System.Collections.Generic;
     using System.Runtime.Serialization;
     using Utilities;
@@ -24,6 +25,50 @@ namespace Microsoft.WindowsAzure.Management.Websites.Services.DeploymentEntities
     [DataContract(Namespace = UriElements.ServiceNamespace)]
     public class Deployment
     {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "status")]
+        public int Status { get; set; }
+
+        [DataMember(Name = "status_text")]
+        public string StatusText { get; set; }
+
+        [DataMember(Name = "author_email")]
+        public string AuthorEmail { get; set; }
+
+        [DataMember(Name = "author")]
+        public string Author { get; set; }
+
+        [DataMember(Name = "deployer")]
+        public string Deployer { get; set; }
+
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+
+        [DataMember(Name = "received_time")]
+        public DateTime ReceivedTime { get; set; }
+
+        [DataMember(Name = "start_time")]
+        public DateTime StartTime { get; set; }
+
+        [DataMember(Name = "end_time")]
+        public DateTime EndTime { get; set; }
+
+        [DataMember(Name = "last_success_end_time")]
+        public DateTime LastSuccessEndTime { get; set; }
+
+        [DataMember(Name = "complete")]
+        public bool Complete { get; set; }
+
+        [DataMember(Name = "active")]
+        public bool Active { get; set; }
+
+        [DataMember(Name = "url")]
+        public string Url { get; set; }
+
+        [DataMember(Name = "log_url")]
+        public string LogUrl { get; set; }
     }
 
     /// <summary>
